bb7ba1d0bfdf9708a68dd446f7195b41804ecc7d
Chinnadurai7 <113617990+Chinnadurai7@users.noreply.github.com>
2024-03-13 09:26:18 +0530

    Add files via upload

---
+++ forgot-password.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forgot Password</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <style>
+    body{
+            background-image: url(lion-roar-colorful-digital-art-animal-4k-wallpaper-uhdpaper.com-614@1@k.jpg);
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+        </style>
+</head>
+<body>
+
+    <h1>Forgot Password</h1>
+
+    <form method="post" action="send-password-reset.php">
+
+        <label for="email">email</label>
+        <input type="email" name="email" id="email">
+
+        <button>Send</button>
+
+    </form>
+
+</body>
+</html>
